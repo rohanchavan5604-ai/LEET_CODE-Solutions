ed22cd96bc7d87940e5629a5209df598e5bc7eb2
Rohan Arun Chavan <rohanchavan5604@gmail.com>
2026-08-06 14:39:04 +0530

    Time: 2 ms (100%), Space: 46.4 MB (63.21%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public int getValue(char ch) {
+
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+
+        return 0;
+    }
+
+    public int romanToInt(String s) {
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int current = getValue(s.charAt(i));
+
+            if (i < s.length() - 1 && current < getValue(s.charAt(i + 1))) {
+                sum = sum - current;
+            } else {
+                sum = sum + current;
+            }
+        }
+
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
