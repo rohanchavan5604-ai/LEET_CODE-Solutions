375b13315626052ba9c4f6ceab72d578094856e4
Rohan Arun Chavan <rohanchavan5604@gmail.com>
2026-08-14 22:37:21 +0530

    Time: 5 ms (8.95%), Space: 45.3 MB (5.01%) - LeetHub - Time: O(max(m, n)), Space: O(max(m, n))

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String addBinary(String a, String b) {
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        String result = "";
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result = (sum % 2) + result;
+            carry = sum / 2;
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
