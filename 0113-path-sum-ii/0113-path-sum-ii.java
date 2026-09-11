cf4c5ebb2f184e999ff2814cfa3cd7e9492d5c75
Rohan Arun Chavan <rohanchavan5604@gmail.com>
2026-09-11 16:28:17 +0530

    Time: 1 ms (99.99%), Space: 45.4 MB (73.11%) - LeetHub - Time: O(N^2), Space: O(N)

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(root, targetSum, path, result);
+
+        return result;
+    }
+
+    private void dfs(TreeNode root, int targetSum,
+                     List<Integer> path,
+                     List<List<Integer>> result) {
+
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.val);
+        targetSum -= root.val;
+
+        if (root.left == null && root.right == null && targetSum == 0) {
+            result.add(new ArrayList<>(path));
+        }
+
+        dfs(root.left, targetSum, path, result);
+        dfs(root.right, targetSum, path, result);
+
+        path.remove(path.size() - 1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
