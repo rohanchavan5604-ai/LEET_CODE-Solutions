c9d0055bac8e2df38cd506d66d609dd55bc917b9
Rohan Arun Chavan <rohanchavan5604@gmail.com>
2026-09-12 09:53:40 +0530

    Time: 8 ms (60.38%), Space: 42.9 MB (70.44%) - LeetHub - Time: O(2^n), Space: O(2^n)

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String countAndSay(int n) {
+        
+        String result = "1";
+
+        for(int i=2; i<=n; i++){
+
+            StringBuilder current =  new StringBuilder();
+            int count = 1;
+
+            for(int j=1; j<result.length(); j++){
+                if(result.charAt(j) == result.charAt(j-1)){
+                    count ++;
+                }else{
+                    current.append(count);
+                    current.append(result.charAt(j-1));
+                    count = 1;
+                }
+            }
+
+
+            current.append(count);
+            current.append(result.charAt(result.length()-1));
+
+            result = current.toString();
+
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
