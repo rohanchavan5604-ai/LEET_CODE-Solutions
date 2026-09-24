8a5028d6591a4732e9222629425a0a4148b48659
Rohan Arun Chavan <rohanchavan5604@gmail.com>
2026-09-24 10:30:26 +0530

    Time: 0 ms (100%), Space: 42.9 MB (85.3%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> res = new ArrayList();
+        temp(res,root);
+        return res;
+
+    }
+
+    private void temp(List<Integer> res, TreeNode root)
+    {
+        if(root == null){
+            return;
+        }
+
+        res.add(root.val);
+        temp(res, root.left);
+        temp(res, root.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
